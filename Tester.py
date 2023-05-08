0b64e91283a8025a46e3b7593e1aee768c34f5e7
Niley GF <87028040+NileyGF@users.noreply.github.com>
2023-05-08 10:47:31 -0400

    Update Tester.py

--- Tester.py
+++ Tester.py
@@ -47,6 +47,7 @@ def Solve_and_Compare(solver:str, read_from:str, save_to:str, compare:bool, comp
     solvers = {'vertices':Vertices_Solver.solver_1,
                'edges_backtrack_td':Edges_Solver.top_down_solver,
                'edges_backtrack_bu':Edges_Solver.bottom_up_solver,
+               'mix_backtrack':Edges_Solver.mix_solver,
                'matching':Matching_Solver.Solver}
     solver = solvers[solver]
     test_cases_f = open(read_from,'rb')
@@ -65,6 +66,7 @@ def Solve_and_Compare(solver:str, read_from:str, save_to:str, compare:bool, comp
         et = time.time()
         duration = round(et-st,5)
         result.append((solution,duration))
+        print(duration,'  sec.')
         if compare:
             for k in solution:
                 if other_res[i][0][k][1] != solution[k][1]:
@@ -78,7 +80,66 @@ def Solve_and_Compare(solver:str, read_from:str, save_to:str, compare:bool, comp
     save_f = open(save_to,'wb')
     pickle.dump(result,save_f)
     save_f.close()
-                    
-Generate_and_Save_Test_Cases(50,6,30)
-Solve_and_Compare('matching','test_cases.bin','matching_solution.bin',False,None)
-Solve_and_Compare('edges_backtrack_td','test_cases.bin','backtrack_td_solution.bin',True,'matching_solution.bin')
+
+"""Generate test cases and solutions, saving them in binary files"""                  
+# Generate_and_Save_Test_Cases(50,6,30)
+# Solve_and_Compare('matching','test_cases.bin','matching_solution.bin',False,None)
+# Solve_and_Compare('mix_backtrack','test_cases.bin','mix_backtrack.bin',True,'matching_solution.bin')
+
+""" Load test cases and solutions and do some printing about them"""  
+# f1 = open('test_cases.bin','rb')
+# graph_list = pickle.load(f1)
+# f1.close()
+
+# f2 = open('matching_solution.bin','rb')
+# matching_sol = pickle.load(f2)
+# f2.close()
+
+# f3 = open('mix_backtrack.bin', 'rb')
+# backtrack_sol = pickle.load(f3)
+# f3.close()
+
+# for i in range(len(graph_list)):
+#     print("i = ",i, " Flow duration : ", str(matching_sol[i][1]), " sec. Backtrack duration : ", " duration : ", str(backtrack_sol[i][1]), " sec.")
+#     # g = graph_list[i]
+#     # print_bip(g,True)
+#     for k in matching_sol[i][0]:
+#        print("\tK = ", str(k), ". Flow solution : ", str(matching_sol[i][0][k][1])) 
+#     #    print_bip(matching_sol[i][0][k][0])
+#        print("\tK = ", str(k), ". Backtrack solution : ", str(backtrack_sol[i][0][k][1])) 
+#     #    print_bip(backtrack_sol[i][0][k][0])
+
+"""Interesting graphs"""
+
+# #   Almost K_35  
+
+# G1 = nx.Graph()
+# A = [1,2,3]
+# B = [4,5,6,7,8]
+# G1.add_nodes_from(A,bipartite=0)
+# G1.add_nodes_from(B,bipartite=1)
+# E = [(1,4), (1,5), (1,6), (2,6), (2,7), (2,8), (3,7), (3,8)]
+# G1.add_edges_from( E )
+
+# #   5 nodes on each side, min_degree = 3
+
+# G2 = nx.Graph()
+
+# A =[1,2,3,4,5]
+# B =[6,7,8,9,10]
+# G2.add_nodes_from(A,bipartite=0)
+# G2.add_nodes_from(B,bipartite=1)
+# E =  [(6,1), (1,7), (1,8), (1,10), (2,7), (2,6), (2,8), (2,9), (3,7), (3,8), (3,9), (4,8), (4,9), (4,10), (5,6), (5,9), (5,10)]
+# G2.add_edges_from( E )
+
+# solution1 = Matching_Solver.Solver(G1)
+# print_bip(G1)
+# for k in solution1:
+#     print(solution1[k][1])
+#     print_bip(solution1[k][0])
+    
+# solution2 = Matching_Solver.Solver(G2)
+# print_bip(G2)
+# for k in solution2:
+#     print(solution2[k][1])
+#     print_bip(solution2[k][0])
